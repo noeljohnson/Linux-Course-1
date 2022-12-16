cf8cdd6fb31c32c1bd13ccd10a733e8982dadd37
Noel Johnson <noel.johnson6213@gmail.com>
2022-12-16 08:02:57 +0530

    Started Chapter 4

--- Chapter 03/intro_to_file.sh
+++ Chapter 03/intro_to_file.sh
@@ -2,4 +2,14 @@
 
 file intro_to_cp.sh #returns the file type
 
+cat intro_to_cp.sh # displays the whole file
+cat -n intro_to_cp.sh #displays the whole file with lines numbers, use -b to count lines only with content
+
+#use more, less commands when the file is more bulky, use less as it has more features
+
+# to display the last lines of files you can use the tail command
+tail intro_to_cp.sh #displays the last ten lines of the file
+tail -n 3 intro_to_cp.sh # display the last three lines 
+# use -f to see the last lines of a file that is continously being updated
+
 ##TYJC
---
+++ Chapter 04/intro_to_ps.sh
@@ -0,0 +1,7 @@
+##GTG
+
+# ps command list process running
+
+ps #simply list the process running on the current terminal by the current user
+
+##TYJC
