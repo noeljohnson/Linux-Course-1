9f41d90e4d160a5ce6248c06f098d6a27f17874b
Noel Johnson <noel.johnson6213@gmail.com>
2022-12-17 08:55:30 +0530

    Continue Chapter 4

--- Chapter 04/intro_to_ps.sh
+++ Chapter 04/intro_to_ps.sh
@@ -4,4 +4,10 @@
 
 ps #simply list the process running on the current terminal by the current user
 
+ps -ef #lists all processes with formatted options
+
+# the pid (process id) in this listing is important
+
+#processes commmunicate with each other using signals
+
 ##TYJC
